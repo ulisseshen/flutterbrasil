9d5c60d890bb048726a285c3aeb13f14d037a011
Ulisses Hen <ulisseshen@gmail.com>
2024-12-27 12:38:20 -0300

    refactor: clean code translator.dart

--- tool/translator/bin/translator.dart
+++ tool/translator/bin/translator.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 import 'package:translator/translator.dart'; // O pacote translator será usado
 
 void main(List<String> arguments) async {
+  const int maxKbSize = 28;
   if (arguments.isEmpty) {
-    print('Usage: flutter-translate <directory> [--info]');
+    print('Usage: flutter-translate <directory> [--info] [-g]');
     exit(1);
   }
 
@@ -17,92 +18,40 @@ void main(List<String> arguments) async {
   final showInfo = arguments.contains('--info');
 
   if (showInfo) {
-    // Mostrar informações dos arquivos no diretório
-    print('Listing files in directory: $directoryPath');
-    await for (var entity
-        in directory.list(recursive: true, followLinks: false)) {
-      if (entity is File && entity.path.endsWith('.md')) {
-        final fileSizeKB = (await entity.length()) / 1024;
-        if (fileSizeKB > 40) {
-          print(
-              'File: ${entity.path}, Size: ${fileSizeKB.toStringAsFixed(2)} KB');
-        }
-      }
-    }
-    exit(0); // Finalizar após mostrar as informações
+    await printDirectoryInfo( directory, maxKbSize);
+    exit(0);
   }
 
-  int fileFoundCount = 0;
-  int skippedCount = 0;
-  int fileGreater = 0;
-  const int limiteSizeFile = 40;
+  final translateGreater = arguments.contains('-g');
 
-  // Contar os arquivos .md antes de começar a tradução
-  await for (var entity
-      in directory.list(recursive: true, followLinks: false)) {
-    if (entity is File && entity.path.endsWith('.md')) {
-      final content = await entity.readAsString();
-
-      if (content.contains('ia-translate: true')) {
-        skippedCount++; // Incrementar contador de arquivos ignorados
-        continue; // Ignorar este arquivo
-      }
-
-      // Verificar se o arquivo tem o header 'ia-translate: true'
-      final fileSizeKB = (await entity.length()) / 1024;
-      if (fileSizeKB > limiteSizeFile) {
-        print('Skipping file > ${limiteSizeFile}KB: ${entity.path}');
-        fileGreater++;
-        continue;
-      }
-
-      fileFoundCount++;
-    }
+  if (translateGreater){
+    
   }
-
-  print('arquivos encontrado: ${skippedCount + fileFoundCount + fileGreater}');
-  print('arquivos já traduzidos: $skippedCount');
-  print('arquivos maior que ${limiteSizeFile}kb: $fileGreater');
-  print('--- arquivos para traduzir: $fileFoundCount ---');
-  final translator = Translator();
-
-  final filesToTranslate = <File>[];
-  int totalSizeKB = 0;
-  // Contar arquivos e filtrar aqueles que devem ser traduzidos
-  await for (var entity
-      in directory.list(recursive: true, followLinks: false)) {
-    if (entity is File && entity.path.endsWith('.md')) {
-      final content = await entity.readAsString();
-
-      // Ignorar arquivos com `ia-translate: true`
-      if (content.contains('ia-translate: true')) {
-        print('Skipping file (ia-translate: true): ${entity.path}');
-        continue;
-      }
-
-      // Ignorar arquivos maiores que 75 KB
-      final fileSizeKB = (await entity.length()) / 1024;
-      if (fileSizeKB > limiteSizeFile) {
-        print('Skipping file > ${limiteSizeFile}KB: ${entity.path}');
-        continue;
-      }
-
-      filesToTranslate.add(entity);
-      totalSizeKB += fileSizeKB.round();
-    }
-  }
-
-  int fileCount = 0;
+  
+  final filesToTranslate = await collectFiles(directory, maxKbSize);
 
   final stopwatchTotal = Stopwatch()..start();
 
-  // Processar arquivos em lotes de 10
+  int fileCount = await translateFIles(filesToTranslate);
+
+  stopwatchTotal.stop();
+
+  print('\n📔Translation Summary:');
+  print('---------------------');
+  print('Total files translated: $fileCount');
+  print('Total translation time: ${stopwatchTotal.elapsed.inSeconds} seconds');
+  print('Translation completed for directory: ${directory.path}');
+}
+
+Future<int> translateFIles(List<File> filesToTranslate) async {
+   int fileCount = 0;
   const batchSize = 10;
+  final translator = Translator();
   for (var i = 0; i < filesToTranslate.length; i += batchSize) {
     final batch = filesToTranslate.skip(i).take(batchSize).toList();
-
+  
     final stopwatchBatch = Stopwatch()..start();
-
+  
     try {
       // Executar as traduções em paralelo
       await Future.wait([
@@ -111,7 +60,7 @@ void main(List<String> arguments) async {
           print(
               'Processing file: ${file.path} (${fileSizeKB.toStringAsFixed(2)} KB)');
           final stopwatchFile = Stopwatch()..start();
-
+  
           try {
             final content = await file.readAsString();
             final translated =
@@ -120,13 +69,13 @@ void main(List<String> arguments) async {
                   'Erro no primeiro modelo: ${file.path} (${fileSizeKB.toStringAsFixed(2)} KB)');
               print('---');
             });
-
+  
             final updatedContent = translated.replaceFirst(
               '---',
               '---\nia-translate: true',
             );
             await file.writeAsString(updatedContent);
-
+  
             // Incrementar contador de arquivos finalizados
             fileCount++;
             print(
@@ -148,14 +97,51 @@ void main(List<String> arguments) async {
       stopwatchBatch.stop();
     }
   }
+  return fileCount;
+}
 
-  stopwatchTotal.stop();
+Future<List<File>> collectFiles(Directory directory, int maxKbSize) async {
+  final filesToTranslate = <File>[];
+  int skippedCount = 0;
+  int fileGreater = 0;
 
-  print('\n📔Translation Summary:');
-  print('---------------------');
-  print('Total files translated: $fileCount');
-  print('Total size of files: ${totalSizeKB.toStringAsFixed(2)} KB');
-  print('Total translation time: ${stopwatchTotal.elapsed.inSeconds} seconds');
+  await for (var entity in directory.list(recursive: true, followLinks: false)) {
+    if (entity is File && entity.path.endsWith('.md')) {
+      final content = await entity.readAsString();
+      final fileSizeKB = (await entity.length()) / 1024;
 
-  print('Translation completed for directory: ${directory.path}');
+      if (content.contains('ia-translate: true')) {
+        skippedCount++;
+        continue;
+      }
+      if (fileSizeKB > maxKbSize) {
+        print('Skipping file > ${maxKbSize}KB: ${entity.path}');
+        fileGreater++;
+        continue;
+      }
+
+      filesToTranslate.add(entity);
+    }
+  }
+
+  print('arquivos encontrado: ${skippedCount + filesToTranslate.length + fileGreater}');
+  print('arquivos já traduzidos: $skippedCount');
+  print('arquivos maior que ${maxKbSize}kb: $fileGreater');
+  print('--- arquivos para traduzir: ${filesToTranslate.length} ---');
+  return filesToTranslate;
+}
+
+Future<void> printDirectoryInfo( Directory directory, int maxKbSize) async {
+   // Mostrar informações dos arquivos no diretório
+  print('Listing files in directory: ${directory.path}');
+  await for (var entity
+      in directory.list(recursive: true, followLinks: false)) {
+    if (entity is File && entity.path.endsWith('.md')) {
+      final fileSizeKB = (await entity.length()) / 1024;
+      if (fileSizeKB > maxKbSize) {
+        print(
+            'File: ${entity.path}, Size: ${fileSizeKB.toStringAsFixed(2)} KB');
+      }
+    }
+  }
 }

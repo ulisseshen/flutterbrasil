3a49db04f347f0da4ac6410b4dfbca3c1761b47e
Ulisses Hen <ulisseshen@gmail.com>
2024-12-28 22:56:58 -0300

    chore: adiciona algoritmo para limpar arquivo que foram envolvidos com markdown-code

--- tool/translator/bin/translator.dart
+++ tool/translator/bin/translator.dart
@@ -23,6 +23,21 @@ void main(List<String> arguments) async {
     exit(0);
   }
 
+  final cleanMarkDown = arguments.contains('-c');
+
+  if (cleanMarkDown) {
+    final all = await collectFilesToclean(directory);
+    for (var i = 0; i < all.length; i++) {
+      final file = all[i];
+      final content = await file.readAsString();
+      final cleanedContent = ensureDontHaveMarkdown(content);
+      // Escrever o conteúdo atualizado no arquivo
+      await file.writeAsString(cleanedContent);
+    }
+    print('ARQUIVOS LIMPO');
+    return;
+  }
+
   final translateGreater = arguments.contains('-g');
 
   final filesToTranslate =
@@ -98,7 +113,12 @@ Future<int> translateFiles(
                   '---',
                   '---\nia-translate: true',
                 );
-            await file.writeAsString(updatedContent);
+
+            // Remover as marcações de markdown
+            final cleanedContent = ensureDontHaveMarkdown(updatedContent);
+
+            // Escrever o conteúdo atualizado no arquivo
+            await file.writeAsString(cleanedContent);
 
             fileCount++;
             print(
@@ -176,3 +196,62 @@ Future<void> printDirectoryInfo(Directory directory, int maxKbSize) async {
     }
   }
 }
+
+/// Remove as marcações ```markdown da primeira linha e ``` da última ou penúltima linha do conteúdo.
+String ensureDontHaveMarkdown(String content) {
+  // Dividir o conteúdo em linhas
+  final lines = content.split('\n');
+
+  // Remover ```markdown da primeira linha, se existir
+  if (lines.isNotEmpty && hasMarwdown(lines.first)) {
+    lines.removeAt(0);
+  }
+
+  // Remover ``` da última linha, ou penúltima se a última for vazia
+  if (lines.isNotEmpty) {
+    final lastLineIndex = lines.length - 1;
+    if (lines[lastLineIndex].trim().isEmpty) {
+      // Se a última linha for vazia, verificar a penúltima
+      if (lastLineIndex > 0 && lines[lastLineIndex - 1].contains('```')) {
+        // lines[lastLineIndex - 1] =
+        //     lines[lastLineIndex - 1].replaceFirst('```', '').trim();
+        lines.removeAt(lastLineIndex - 1);
+      }
+    } else {
+      // Se a última linha não for vazia, verificar a última linha
+      if (lines[lastLineIndex].contains('```')) {
+        // lines[lastLineIndex] =
+        //     lines[lastLineIndex].replaceFirst('```', '').trim();
+        lines.removeAt(lastLineIndex);
+      }
+    }
+  }
+
+  // Recriar o conteúdo com as linhas ajustadas
+  return lines.join('\n');
+}
+
+Future<List<File>> collectFilesToclean(Directory directory) async {
+  final filesToClean = <File>[];
+  int skippedCount = 0;
+
+  await for (var entity
+      in directory.list(recursive: true, followLinks: false)) {
+    if (entity is File && entity.path.endsWith('.md')) {
+      final content = await entity.readAsString();
+      final lines = content.split('\n');
+      if (lines.isNotEmpty && hasMarwdown(lines.first)) {
+        filesToClean.add(entity);
+      } else {
+        skippedCount++;
+      }
+    }
+  }
+
+  print('jã limpos: $skippedCount');
+  print('--- arquivos para limpar: ${filesToClean.length} ---');
+  return filesToClean;
+}
+
+bool hasMarwdown(String line) =>
+    (line.contains('```markdown') || line.contains('```'));
